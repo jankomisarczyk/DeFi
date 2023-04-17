f9f9ef322d6a3a878a054a64b62337f512a83370
jankomisarczyk <101530719+jankomisarczyk@users.noreply.github.com>
2023-04-17 21:26:43 +0200

    Add files via upload

---
+++ Blanco/BlancoToken.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+import "./interfaces/IERC20.sol";
+import "./libraries/SafeMath.sol";
+
+contract BlancoToken is IERC20 {
+  using SafeMath for uint256;
+
+  uint256 public _totalSupply = 10**13;
+  string public name = 'Blanco Token';
+  uint8 public constant decimals = 10;
+  string public constant symbol = 'Blanco';
+  address public highestBalanceAddress;
+
+
+  mapping (address => uint256) private _balances;
+
+  mapping (address => mapping (address => uint256)) private _allowed;
+
+
+  constructor () {
+    _balances[msg.sender] = _totalSupply;
+    highestBalanceAddress = msg.sender;
+    emit Transfer(address(0), msg.sender , _totalSupply);
+  }
+
+  function totalSupply() public override view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address owner) public override view returns (uint256) {
+    return _balances[owner];
+  }
+
+  function changeTokenName(string memory newName) public returns (bool) {
+    require(msg.sender == highestBalanceAddress, "Only the highest token holder can change the name.");
+    name = newName;
+    return true;
+  }
+
+  /**
+  * Function to check the amount of tokens that an owner allowed to a spender.
+  */
+  function allowance(address owner, address spender) public override view returns (uint256){
+    return _allowed[owner][spender];
+  }
+
+  function transfer(address to, uint256 value) public override returns (bool) {
+    require(value <= _balances[msg.sender]);
+    require(to != address(0));
+
+    _balances[msg.sender] = _balances[msg.sender].sub(value);
+    _balances[to] = _balances[to].add(value);
+
+    if (_balances[to] > _balances[highestBalanceAddress]) {
+      highestBalanceAddress = to;
+    }
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function approve(address spender, uint256 value) public override returns (bool) {
+    require(spender != address(0));
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  function _transfer(address from, address to, uint value) private {
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    if (_balances[to] > _balances[highestBalanceAddress]) {
+      highestBalanceAddress = to;
+    }
+    emit Transfer(from, to, value);
+  }
+
+  function transferFrom( address from, address to, uint256 value) public override returns (bool){
+    require(value <= _balances[from]);
+    require(value <= _allowed[from][msg.sender]);
+    require(to != address(0));
+    _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+    _transfer(from, to, value);
+    return true;
+  }
+
+  function increaseAllowance( address spender, uint256 addedValue ) public returns (bool){
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].add(addedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  function decreaseAllowance(address spender,uint256 subtractedValue) public returns (bool){
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].sub(subtractedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  function mint(uint256 value) public returns (bool) {
+      require(msg.sender == highestBalanceAddress, "Only the highest token holder can mint new tokens.");
+
+      _totalSupply = _totalSupply.add(value);
+      _balances[msg.sender] = _balances[msg.sender].add(value);
+
+      emit Transfer(address(0), msg.sender, value);
+      return true;
+  }
+
+  function burn(address to, uint256 value) public returns (bool) {
+      require(to != address(0), "ERC20: burn from the zero address");
+      require(msg.sender == highestBalanceAddress, "Only the highest token holder can punish and burn tokens.");
+      require(value <= _balances[to], "burn amount exceeds balance");
+
+      _totalSupply = _totalSupply.sub(value);
+      _balances[to] = _balances[to].sub(value);
+
+      emit Transfer(to, address(0), value);
+      return true;
+  }
+
+}
---
+++ Blanco/ERC20.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.6.12;
+
+import './libraries/SafeMath.sol';
+
+contract ERC20 {
+    using SafeMath for uint;
+
+    string public constant name = 'Uniswap V2';
+    string public constant symbol = 'UNI-V2';
+    uint8 public constant decimals = 18;
+    uint  public  totalSupply;
+    mapping(address => uint) public  balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    mapping(address => uint) public nonces;
+
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    constructor() public{
+  
+    }
+
+    function _mint(address to, uint value) internal {
+        totalSupply = totalSupply.add(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(address(0), to, value);
+    }
+
+    function _burn(address from, uint value) internal {
+        balanceOf[from] = balanceOf[from].sub(value);
+        totalSupply = totalSupply.sub(value);
+        emit Transfer(from, address(0), value);
+    }
+
+    function _approve(address owner, address spender, uint value) private {
+        allowance[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    function _transfer(address from, address to, uint value) private {
+        balanceOf[from] = balanceOf[from].sub(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    function approve(address spender, uint value) external returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function transfer(address to, uint value) external returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint value) external  returns (bool) {
+        if (allowance[from][msg.sender] != uint(-1)) {
+            allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+        }
+        _transfer(from, to, value);
+        return true;
+    }
+
+}
